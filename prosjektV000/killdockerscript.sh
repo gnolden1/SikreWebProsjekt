48ce77a887e62b4adf8ee70434cbd6c998d2d73b
gnolden <didrik.larsen@gmail.com>
2023-11-17 12:12:21 +0100

    stuff

--- prosjektV000/killdockerscript.sh
+++ prosjektV000/killdockerscript.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 
-sudo docker stop konteiner1
-sudo docker stop konteiner2
-sudo docker stop konteiner3
+#sudo docker stop konteiner1
+#sudo docker stop konteiner2
+#sudo docker stop konteiner3
 
-sudo docker rm konteiner1
-sudo docker rm konteiner2
-sudo docker rm konteiner3
+#sudo docker rm konteiner1
+#sudo docker rm konteiner2
+#sudo docker rm konteiner3
+
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+docker image rm $(sudo docker images)
 
 #sudo docker compose down
 #kan også sudo docker konteiner2 down feks
